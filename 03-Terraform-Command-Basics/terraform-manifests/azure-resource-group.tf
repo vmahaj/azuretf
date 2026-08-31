44154c97bfc761d1440d5f5984013ce2c9f617a1
Vinay Mahajan <mahajan.vinu@gmail.com>
2026-08-30 21:21:46 -0400

    updated terraform

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -1,11 +1,10 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended in production
-    }    
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
   }
 }
 
